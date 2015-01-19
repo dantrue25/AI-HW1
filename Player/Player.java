1c7da66ae2e0a40873a30ce8cb93d6ffff8ce325
Nick <mueschn@gmail.com>
2015-01-19 18:54:01 -0500

    Changes player name.

--- Player/Player.java
+++ Player/Player.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Player {
 
-	String playerName="aa";
+	String playerName="BEST PLAYER EVER";
 	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	boolean first_move=false;
 	
