dec88e70c09989286a9ef6d45ea85f36db7bcba2
Dan True <dbtrue@wpi.edu>
2015-01-21 19:45:12 -0500

    Adds Move implementation.

--- Player/Move.java
+++ Player/Move.java
@@ -8,6 +8,24 @@ package Player;
  *
  */
 public class Move {
+	public int column;
+	public int moveType;
 	
+	/**
+	 * Constructor
+	 * @param column
+	 * @param moveType
+	 */
+	public Move (int column, int moveType) {
+		this.column = column;
+		this.moveType = moveType;
+	}
+	
+	/**
+	 * Used to pass referee our move
+	 */
+	public String toString () {
+		return "" + column + " " + moveType;
+	}
 
 }
