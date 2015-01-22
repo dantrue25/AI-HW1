152c7d4191f7f43866917ed5fbee14e86980fe52
Nick <mueschn@gmail.com>
2015-01-21 19:46:07 -0500

    Adds a Node class.

---
+++ Player/Node.java
@@ -0,0 +1,27 @@
+package Player;
+
+import java.util.ArrayList;
+
+
+/**
+ * 
+ * @author Nick
+ * @author Dan
+ */
+
+public class Node {
+	public double hval;
+	public ArrayList<Move> moves;
+	public ArrayList<Node> children;
+	
+	public Node(int hvals, ArrayList<Move> moves) {
+		this.hval = hvals;
+		this.moves = moves;
+		this.children = null;
+	}
+	
+	public void addChild(int hval, ArrayList<Move> moves){
+		children.add(new Node(hval, moves));
+	}
+	
+}
